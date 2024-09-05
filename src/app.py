c4801ecb7f3e6dadf7209c561c74162685161ce3
vitris128 <lenhattrong128@gmail.com>
2024-09-06 05:06:48 +0700

    Init python web server with Flask

---
+++ src/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template
+import joblib
+
+
+app = Flask(__name__)
+
+# Load the trained model and Iris dataset
+model = joblib.load('svm_model.pkl')
+label_names = ['setosa', 'versicolor', 'virginica']
+
+def predict_iris_label(petal_length, petal_width, sepal_length, sepal_width):
+    features = [[petal_length, petal_width, sepal_length, sepal_width]]
+    prediction = model.predict(features)
+    predicted_label = prediction[0]
+    predicted_class = label_names[predicted_label]
+    return predicted_label, predicted_class
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            petal_length = float(request.form['petal_length'])
+            petal_width = float(request.form['petal_width'])
+            sepal_length = float(request.form['sepal_length'])
+            sepal_width = float(request.form['sepal_width'])
+
+            predicted_label, predicted_class = predict_iris_label(petal_length, petal_width, sepal_length, sepal_width)
+            result_label = predicted_label
+            result_class = predicted_class
+            result = f"Predicted label: {result_label}, Predicted class: {result_class}"
+        except ValueError:
+            result_label = result_class = None
+            result = "Please enter valid numerical values."
+
+        return render_template('index.html', result=result, result_label=result_label, result_class=result_class)
+
+    return render_template('index.html', result=None, result_label=None, result_class=None)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
---
+++ src/training.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn import svm
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import joblib
+
+iris = datasets.load_iris()
+columns=["Petal length","Petal Width","Sepal Length","Sepal Width"]
+x = pd.DataFrame(iris.data, columns=columns)
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+model = svm.SVC(kernel='rbf')
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Accuracy: %.3f" %model.score(X_test, y_pred))
+
+joblib.dump(model, 'svm_model.pkl')
